4a38579cd990885cfa431ea458bff70bf8727f77
user <user@user-HP-Pro-3330-MT.(none)>
2013-07-05 13:37:24 +0800

    delete resolveIPvt.py

--- resolveIPv6.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def resolveUrl(url):
-    return socket.getaddrinfo(url,None)
-    
-if __name__=='__main__':
-    url='ipv6.google.com'
-    result=resolveUrl(url)
-    print result[0][4][0]
